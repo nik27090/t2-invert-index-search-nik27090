109b7cb230b051a1b7dd78cf7b77706b1f9970b4
Nikita Homenko <44022430+nik27090@users.noreply.github.com>
2019-04-04 21:59:59 +0300

    Add files via upload

---
+++ invertIndex/invertIndex(B)_test.go
@@ -0,0 +1,23 @@
+package invertIndex
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+// go test -benchmem -bench .
+
+var sliceFiles2 []File
+
+func init() {
+	f := File{Name: "time.txt", Content: ""}
+	file, _ := ioutil.ReadFile(f.Name)
+	f.Content = string(file)
+	sliceFiles2 = append(sliceFiles2, f)
+}
+
+func BenchmarkInvertInd(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PreInvertIndex(sliceFiles2)
+	}
+}
